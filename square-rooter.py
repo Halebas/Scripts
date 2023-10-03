71c71a82aeb88ce0bbb5124cec6e47c2c970a55d
Halebas <ggpoletaev@gmail.com>
2023-10-03 19:34:14 +0700

    add complex roots finder

--- square-rooter.py
+++ square-rooter.py
@@ -2,14 +2,17 @@ a, b, c = map(int, input("Enter a, b and c: ").split())
 print(f"\n{a}x^2 + {b}x + {c} = 0")
 
 D = b**2 - 4 * a * c
-print(f"D = {D}")
+print(f"{D = }")
 
 if D == 0:
     x = -b / (2 * a)
-    print(f"x = {x}")
+    print(f"{x = }")
 elif D > 0:
     x1 = (-b + D**0.5) / (2 * a)
     x2 = (-b - D**0.5) / (2 * a)
-    print(f"x1 = {x1}\nx2 = {x2}")
+    print(f"{x1 = }\n{x2 = }")
 else:
+    x1 = (-b + D**0.5) / (2 * a)
+    x2 = (-b - D**0.5) / (2 * a)
     print("No real roots...")
+    print(f"Complex roots:\n{x1 = :.5f}\n{x2 = :.5f}")
