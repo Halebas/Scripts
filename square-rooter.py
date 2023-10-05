d85570425debba6a2ed2a941ad815fa6732051b1
Halebas <ggpoletaev@gmail.com>
2023-10-05 19:57:52 +0700

    make silent mode for square-rooter

--- square-rooter.py
+++ square-rooter.py
@@ -1,19 +1,20 @@
-def solve_quadratic_equation(a: int, b: int, c: int):
-    print(f"\n{a}x^2 + {b}x + {c} = 0")
-
+def solve_quadratic_equation(a: int, b: int, c: int, is_silent=False):
     D = b**2 - 4 * a * c
-    print(f"{D = }")
 
     x1 = (-b + D**0.5) / (2 * a)
     x2 = (-b - D**0.5) / (2 * a)
 
-    if D == 0:
-        print(f"x = {x1}")
-    elif D > 0:
-        print(f"{x1 = }\n{x2 = }")
-    else:
-        print("No real roots...")
-        print(f"Complex roots:\n{x1 = :.5f}\n{x2 = :.5f}")
+    if not is_silent:
+        print(f"\n{a}x^2 + {b}x + {c} = 0")
+        print(f"{D = }")
+
+        if D == 0:
+            print(f"x = {x1}")
+        elif D > 0:
+            print(f"{x1 = }\n{x2 = }")
+        else:
+            print("No real roots...")
+            print(f"Complex roots:\n{x1 = :.5f}\n{x2 = :.5f}")
 
     return (x1, x2)
 
