a94cae0ae28527546962e1ae7e6bd6b695ae8024
Halebas <ggpoletaev@gmail.com>
2023-10-05 19:34:07 +0700

    simplify square-rooter.py

--- square-rooter.py
+++ square-rooter.py
@@ -4,15 +4,13 @@ print(f"\n{a}x^2 + {b}x + {c} = 0")
 D = b**2 - 4 * a * c
 print(f"{D = }")
 
+x1 = (-b + D**0.5) / (2 * a)
+x2 = (-b - D**0.5) / (2 * a)
+
 if D == 0:
-    x = -b / (2 * a)
-    print(f"{x = }")
+    print(f"x = {x1}")
 elif D > 0:
-    x1 = (-b + D**0.5) / (2 * a)
-    x2 = (-b - D**0.5) / (2 * a)
     print(f"{x1 = }\n{x2 = }")
 else:
-    x1 = (-b + D**0.5) / (2 * a)
-    x2 = (-b - D**0.5) / (2 * a)
     print("No real roots...")
     print(f"Complex roots:\n{x1 = :.5f}\n{x2 = :.5f}")
