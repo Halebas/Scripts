25920f0b0aa16242f5f19c2eada83f8c1af52423
Halebas <ggpoletaev@gmail.com>
2023-10-05 20:09:52 +0700

    add incorrect input check to square-rooter.py

--- square-rooter.py
+++ square-rooter.py
@@ -1,4 +1,9 @@
 def solve_quadratic_equation(a: int, b: int, c: int, is_silent=False):
+    if a == 0:
+        if not is_silent:
+            print("Wrong input: a = 0")
+        return (None, None)
+
     D = b**2 - 4 * a * c
 
     x1 = (-b + D**0.5) / (2 * a)
